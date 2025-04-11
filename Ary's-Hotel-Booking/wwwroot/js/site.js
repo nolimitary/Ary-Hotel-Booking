3665bab2fc562d47cf674a3c3bf4316d95ac7324
chris <christianangelovv@gmail.com>
2025-04-11 12:50:03 +0200

    Add project files.

---
+++ Ary's-Hotel-Booking/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿function toggleDatePicker() {
+    const picker = document.getElementById('customDatePicker');
+    picker.classList.toggle('show');
+}
+
+function applyDates() {
+    const checkIn = document.getElementById('checkIn').value;
+    const checkOut = document.getElementById('checkOut').value;
+
+    const dateInput = document.getElementById('dateRangePicker');
+    if (checkIn && checkOut) {
+        dateInput.value = `${checkIn} to ${checkOut}`;
+        toggleDatePicker();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('dateRangePicker');
+    if (input) {
+        input.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleDatePicker();
+        });
+    }
+});
+
+
