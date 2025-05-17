221037e5a87ee1c78e7a9bcdf494dbec3727a16c
Ary <sehgalary@gmail.com>
2025-05-17 19:31:49 +0300

    feat: Add initial EF migration and configure design-time DbContext factory

--- Aryaans Hotel Booking/Program.cs
+++ Aryaans Hotel Booking/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore; 
+using Aryaans_Hotel_Booking.Data;   
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
